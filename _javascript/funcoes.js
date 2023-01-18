7a75e17c4b670194b00d70f978931c4580e10723
kaique <kaqukakk@gmail.com>
2023-01-18 13:59:13 -0300

    Projeto-Google-Glass

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+function mudafoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+function calc_total() {
+    var qtd = parseInt(document.getElementById('cqtd').value);
+    tot = qtd * 1500;
+    document.getElementById('ctot').value = tot
+}
